51f67adf2eb01dc9e1a622894444bc49e5cf5d52
Pratish Ninawe <pratish.1006@gmail.com>
2024-12-11 22:17:02 +0530

    fix:Suspense boundry error

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,13 +2,14 @@
 import { ThemeProvider } from './theme-provider';
 import { QueryProvider } from './query-provider';
 import { RecoilRoot } from 'recoil';
+import { Suspense } from 'react';
 
 export const Providers = ({ children }: { children: React.ReactNode }): JSX.Element => {
 	return (
 		<RecoilRoot>
 			<QueryProvider>
 				<ThemeProvider attribute='class' defaultTheme='light'>
-					{children}
+					<Suspense>{children}</Suspense>
 				</ThemeProvider>
 			</QueryProvider>
 		</RecoilRoot>
